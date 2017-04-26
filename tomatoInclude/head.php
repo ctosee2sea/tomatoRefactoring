3255b0a0e9174b324051626759571f8cf2531301
Sean Oh <esil@tinkl.com>
2017-04-27 03:55:16 +0900

    remove Composer for reInstall

--- tomatoInclude/head.php
+++ tomatoInclude/head.php
@@ -1,7 +1,6 @@
 <?php
 use udemzisoft\tomato as ut;
 
-
 session_start();
 session_cache_limiter('private');
 ini_set("session.cookie_lifetime", "86400");
@@ -9,29 +8,29 @@ ini_set("session.cache_expire", "86400");
 ini_set("session.gc_maxlifetime", "86400");
 ini_set('display_errors', 1);
 
-define ("ROOT_WEBDIR","");
-define ("ROOT_DIR",$_SERVER["DOCUMENT_ROOT"]);
-define ("TOMATO_DIR",$_SERVER["DOCUMENT_ROOT"].'/tomatoInclude');
-define ("TIME_START",microtime(true));
+define("ROOT_WEBDIR", "");
+define("ROOT_DIR", $_SERVER["DOCUMENT_ROOT"]);
+define("TOMATO_DIR", $_SERVER["DOCUMENT_ROOT"].'/tomatoInclude');
+define("TIME_START", microtime(true));
 
 require TOMATO_DIR.'/loader.php';
 require TOMATO_DIR.'/config.php';
 require TOMATO_DIR.'/func.php';
 
 $DB = ut\db::connect([
-		'type'=>DB_TYPE,
-		'dbname'=>DB_NAME,
-		'port'=>DB_PORT,
-		'user'=>DB_USER,
-		'charset'=>DB_CHARSET,
-		'pass'=>DB_PASS,
-		'host'=>DB_HOST
-	]);
+        'type'=>DB_TYPE,
+        'dbname'=>DB_NAME,
+        'port'=>DB_PORT,
+        'user'=>DB_USER,
+        'charset'=>DB_CHARSET,
+        'pass'=>DB_PASS,
+        'host'=>DB_HOST
+    ]);
 $QUICKLOAD_CACHE = new ut\kvCache();
 $QUICKLOAD = new ut\quickLoad($DB);
 $AUTHORITY = new ut\authority([
-		"Id"=>1
-	],$DB);
+        "Id"=>1
+    ], $DB);
 
 // $B = getpost::int("bId");
 // $A = getpost::multi(["bId","mode","id"]);
